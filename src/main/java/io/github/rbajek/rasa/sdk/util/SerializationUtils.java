e68032ab64ce40c6e71d6080a35188b65feaf6ea
Rafał Bajek <rafal.bajek@cometari.com>
2019-11-07 14:26:53 +0100

    Removed unused classes and commented methods in SerializationUtils class

--- src/main/java/io/github/rbajek/rasa/sdk/util/SerializationUtils.java
+++ src/main/java/io/github/rbajek/rasa/sdk/util/SerializationUtils.java
@@ -9,6 +9,13 @@ public class SerializationUtils {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    /**
+     * Clone deep using JSON serialization
+     *
+     * @param object an object which should be cloned
+     * @param <T> type of the object which should be cloned
+     * @return the cloned object
+     */
     public static <T> T deepClone(final T object) {
         try {
             return (T) OBJECT_MAPPER.readValue(OBJECT_MAPPER.writeValueAsString(object), object.getClass());
@@ -17,13 +24,20 @@ public class SerializationUtils {
         }
     }
 
+    /**
+     * Clone deep using Java serialization
+     *
+     * @param object an object which should be cloned
+     * @param <T> type of the object which should be cloned
+     * @return the cloned object
+     */
     public static <T extends Serializable> T deepClone(final T object)  {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         serialize(object, baos);
         return deserialize(new ByteArrayInputStream(baos.toByteArray()));
     }
 
-    public static void serialize(final Serializable obj, final OutputStream outputStream) {
+    private static void serialize(final Serializable obj, final OutputStream outputStream) {
         try (ObjectOutputStream out = new ObjectOutputStream(outputStream)) {
             out.writeObject(obj);
         } catch (final IOException ex) {
@@ -31,7 +45,7 @@ public class SerializationUtils {
         }
     }
 
-    public static <T> T deserialize(final InputStream inputStream) {
+    private static <T> T deserialize(final InputStream inputStream) {
         try (ObjectInputStream in = new ObjectInputStream(inputStream)) {
             return (T) in.readObject();
         } catch (IOException | ClassNotFoundException e) {
